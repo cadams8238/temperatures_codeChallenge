509074348b78e303005d471aa6298a78f4d1f6d3
Courtney Adams <cadams8238@gmail.com>
2018-10-11 11:29:14 -0700

    updated reducer so that it clears previous error in state when a new weather request is made. This allows user to reenter a search and have data displayed (if search is valid) after they entered an invalid search.

--- src/actions/weatherData.js
+++ src/actions/weatherData.js
@@ -1,105 +1,101 @@
-import moment from 'moment';
+import moment from "moment";
 export const BASE_API_URL = "http://api.apixu.com/v1/history.json?key=";
 
-
-export const FETCH_WEATHER_DATA_REQUEST = 'FETCH_WEATHER_DATA_REQUEST'
+export const FETCH_WEATHER_DATA_REQUEST = "FETCH_WEATHER_DATA_REQUEST";
 const fetchWeatherDataRequest = () => ({
-  type: FETCH_WEATHER_DATA_REQUEST
-})
+	type: FETCH_WEATHER_DATA_REQUEST
+});
 
-export const FETCH_WEATHER_DATA_SUCCESS = 'FETCH_WEATHER_DATA_SUCCESS';
+export const FETCH_WEATHER_DATA_SUCCESS = "FETCH_WEATHER_DATA_SUCCESS";
 const fetchWeatherDataSuccess = data => ({
-  type: FETCH_WEATHER_DATA_SUCCESS,
-  data
-})
+	type: FETCH_WEATHER_DATA_SUCCESS,
+	data
+});
 
-export const FETCH_WEATHER_DATA_ERROR = 'FETCH_WEATHER_DATA_ERROR';
+export const FETCH_WEATHER_DATA_ERROR = "FETCH_WEATHER_DATA_ERROR";
 const fetchWeatherDataError = error => ({
-  type: FETCH_WEATHER_DATA_ERROR,
-  error
-})
+	type: FETCH_WEATHER_DATA_ERROR,
+	error
+});
 
 export const encodeSpaces = searchTerm => {
-  let encodedSearch = '';
+	let encodedSearch = "";
 
-  for (let i = 0; i < searchTerm.length; i++) {
-    if (searchTerm[i] === ' ') {
-      encodedSearch += '%20';
-    }
-    else {
-      encodedSearch += searchTerm[i];
-    }
-  }
-  return encodedSearch;
-}
+	for (let i = 0; i < searchTerm.length; i++) {
+		if (searchTerm[i] === " ") {
+			encodedSearch += "%20";
+		} else {
+			encodedSearch += searchTerm[i];
+		}
+	}
+	return encodedSearch;
+};
 
 export const getWeatherHistoryDates = () => {
-  let dates = [];
+	let dates = [];
 
-  for (let i = 0; i < 7; i++) {
-    if (i === 0) {
-      dates.push(moment().format('YYYY-M-D'))
-    }
-    else {
-      dates.push(moment().subtract(i, 'days').format('YYYY-M-D'))
-    }
-  }
-  return dates;
-}
+	for (let i = 0; i < 7; i++) {
+		if (i === 0) {
+			dates.push(moment().format("YYYY-M-D"));
+		} else {
+			dates.push(
+				moment()
+					.subtract(i, "days")
+					.format("YYYY-M-D")
+			);
+		}
+	}
+	return dates;
+};
 
 export const createURLs = (dates, search) => {
-  return dates.map(date => `${BASE_API_URL}${process.env.REACT_APP_API_KEY}&q=${search}&dt=${date}`);
-}
+	return dates.map(
+		date =>
+			`${BASE_API_URL}${process.env.REACT_APP_API_KEY}&q=${search}&dt=${date}`
+	);
+};
 
-export const aggregateData = (apiResponse) => {
-  let data = {};
+export const aggregateData = apiResponse => {
+	let data = {};
 
-  for (let i = apiResponse.length - 1; i >= 0; i--) {
-    if (i === apiResponse.length - 1) {
-      data = {
-        name: apiResponse[i].location.name,
-        region: apiResponse[i].location.region,
-        country: apiResponse[i].location.country,
-        temps: [
-          {
-            date: apiResponse[i].forecast.forecastday[0].date,
-            maxtemp_c: apiResponse[i].forecast.forecastday[0].day.maxtemp_c,
-            mintemp_c: apiResponse[i].forecast.forecastday[0].day.mintemp_c,
-            maxtemp_f: apiResponse[i].forecast.forecastday[0].day.maxtemp_f,
-            mintemp_f: apiResponse[i].forecast.forecastday[0].day.mintemp_f
-          }
-        ]
-      }
-    }
-    else {
-      data.temps.push({
-        date: apiResponse[i].forecast.forecastday[0].date,
-        maxtemp_c: apiResponse[i].forecast.forecastday[0].day.maxtemp_c,
-        mintemp_c: apiResponse[i].forecast.forecastday[0].day.mintemp_c,
-        maxtemp_f: apiResponse[i].forecast.forecastday[0].day.maxtemp_f,
-        mintemp_f: apiResponse[i].forecast.forecastday[0].day.mintemp_f
-      })
-    }
-  }
-  return data;
-}
-
-
+	for (let i = apiResponse.length - 1; i >= 0; i--) {
+		if (i === apiResponse.length - 1) {
+			data = {
+				name: apiResponse[i].location.name,
+				region: apiResponse[i].location.region,
+				country: apiResponse[i].location.country,
+				temps: [
+					{
+						date: apiResponse[i].forecast.forecastday[0].date,
+						maxtemp_c: apiResponse[i].forecast.forecastday[0].day.maxtemp_c,
+						mintemp_c: apiResponse[i].forecast.forecastday[0].day.mintemp_c,
+						maxtemp_f: apiResponse[i].forecast.forecastday[0].day.maxtemp_f,
+						mintemp_f: apiResponse[i].forecast.forecastday[0].day.mintemp_f
+					}
+				]
+			};
+		} else {
+			data.temps.push({
+				date: apiResponse[i].forecast.forecastday[0].date,
+				maxtemp_c: apiResponse[i].forecast.forecastday[0].day.maxtemp_c,
+				mintemp_c: apiResponse[i].forecast.forecastday[0].day.mintemp_c,
+				maxtemp_f: apiResponse[i].forecast.forecastday[0].day.maxtemp_f,
+				mintemp_f: apiResponse[i].forecast.forecastday[0].day.mintemp_f
+			});
+		}
+	}
+	return data;
+};
 
 export const fetchWeatherData = searchTerm => dispatch => {
-  dispatch(fetchWeatherDataRequest())
+	dispatch(fetchWeatherDataRequest());
 
-  const search = encodeSpaces(searchTerm);
-  const weatherHistoryDates = getWeatherHistoryDates();
-  const urls = createURLs(weatherHistoryDates, search);
+	const search = encodeSpaces(searchTerm);
+	const weatherHistoryDates = getWeatherHistoryDates();
+	const urls = createURLs(weatherHistoryDates, search);
 
-  return (
-    Promise.all(urls.map(url =>
-      fetch(url)
-        .then(res => res.json())
-    ))
-      .then(res => aggregateData(res))
-      .then(data => dispatch(fetchWeatherDataSuccess(data)))
-      .catch(err => dispatch(fetchWeatherDataError(err)))
-  );
-}
+	return Promise.all(urls.map(url => fetch(url).then(res => res.json())))
+		.then(res => aggregateData(res))
+		.then(data => dispatch(fetchWeatherDataSuccess(data)))
+		.catch(err => dispatch(fetchWeatherDataError(err)));
+};
--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -1,37 +1,37 @@
 import {
-  FETCH_WEATHER_DATA_REQUEST,
-  FETCH_WEATHER_DATA_SUCCESS,
-  FETCH_WEATHER_DATA_ERROR
-} from '../actions/weatherData';
+	FETCH_WEATHER_DATA_REQUEST,
+	FETCH_WEATHER_DATA_SUCCESS,
+	FETCH_WEATHER_DATA_ERROR
+} from "../actions/weatherData";
 
 const initialState = {
-  data: null,
-  loading: false,
-  error: null
-}
+	data: null,
+	loading: false,
+	error: null
+};
 
 const weatherReducer = (state = initialState, action) => {
-  if (action.type === FETCH_WEATHER_DATA_REQUEST) {
-    return {
-      ...state,
-      loading: true
-    };
-  }
-  else if (action.type === FETCH_WEATHER_DATA_SUCCESS) {
-    return {
-      ...state,
-      loading: false,
-      data: action.data
-    };
-  }
-  else if (action.type === FETCH_WEATHER_DATA_ERROR) {
-    return {
-      ...state,
-      loading: false,
-      error: action.error
-    };
-  }
-  return state;
-}
+	if (action.type === FETCH_WEATHER_DATA_REQUEST) {
+		return {
+			...state,
+			loading: true,
+			data: null,
+			error: null
+		};
+	} else if (action.type === FETCH_WEATHER_DATA_SUCCESS) {
+		return {
+			...state,
+			loading: false,
+			data: action.data
+		};
+	} else if (action.type === FETCH_WEATHER_DATA_ERROR) {
+		return {
+			...state,
+			loading: false,
+			error: action.error
+		};
+	}
+	return state;
+};
 
 export default weatherReducer;
