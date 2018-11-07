57bd1eefad9fb3b9d16c1a946322bc7fba8a42ba
Courtney Adams <cadams8238@gmail.com>
2018-11-07 13:37:06 -0800

    refactored Dashboard component tests

--- src/__tests__/Dashboard.test.js
+++ src/__tests__/Dashboard.test.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import Dashboard from '../components/barGraph';
-import { shallow } from 'enzyme';
+import React from "react";
+import Dashboard from "../components/barGraph";
+import { mount } from "enzyme";
 
-describe('<Dashboard />', () => {
-  it('Should render without crashing', () => {
-    const dispatch = jest.fn();
-    shallow(<Dashboard dispatch={dispatch}/>);
-  })
-})
+describe("<Dashboard />", () => {
+	describe("rendering", () => {
+		let props, wrapper;
+
+		beforeEach(() => {
+			props = {
+				dispatch: jest.fn()
+			};
+
+			wrapper = mount(<Dashboard {...props} />);
+		});
+
+		it("Should render without crashing", () => {
+			wrapper;
+		});
+	});
+});
