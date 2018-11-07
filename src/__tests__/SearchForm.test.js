47aa9a6b3c42c157cedeb5563cafa866e9391613
Courtney Adams <cadams8238@gmail.com>
2018-11-07 10:54:59 -0800

    refactored test structure for SearchForm component to be easier to read when tests run. added test to make sure an input element exists

--- src/__tests__/SearchForm.test.js
+++ src/__tests__/SearchForm.test.js
@@ -2,40 +2,48 @@ import React from "react";
 import SearchForm from "../components/searchForm";
 import { mount } from "enzyme";
 
-describe("SearchForm component", () => {
-	let props, wrapper;
+describe("<SearchForm />", () => {
+	describe("rendering", () => {
+		let props, wrapper;
 
-	beforeEach(() => {
-		props = {
-			updateSearchTerm: undefined,
-			fetchData: undefined,
-			searchTerm: undefined
-		};
+		beforeEach(() => {
+			props = {
+				updateSearchTerm: undefined,
+				fetchData: undefined,
+				searchTerm: undefined
+			};
 
-		wrapper = mount(<SearchForm {...props} />);
+			wrapper = mount(<SearchForm {...props} />);
+		});
+
+		it("Should render without crashing", () => {
+			wrapper;
+		});
+
+		it("Should render a form", () => {
+			expect(wrapper.find("form")).toHaveLength(1);
+		});
+
+		it("Should contain an input element", () => {
+			expect(wrapper.find("input")).toHaveLength(1);
+		});
 	});
 
-	it("Should render without crashing", () => {
-		wrapper;
-	});
+	describe("behavior", () => {
+		it("Should call function passed in on button click", () => {
+			const updateSearchTerm = jest.fn(),
+				fetchData = jest.fn(),
+				searchTerm = "",
+				component = mount(
+					<SearchForm
+						updateSearchTerm={updateSearchTerm}
+						fetchData={fetchData}
+						searchTerm={searchTerm}
+					/>
+				);
 
-	it("Should render a form", () => {
-		expect(wrapper.find("form")).toHaveLength(1);
-	});
-
-	it("Should call function passed in on button click", () => {
-		const updateSearchTerm = jest.fn(),
-			fetchData = jest.fn(),
-			searchTerm = "",
-			component = mount(
-				<SearchForm
-					updateSearchTerm={updateSearchTerm}
-					fetchData={fetchData}
-					searchTerm={searchTerm}
-				/>
-			);
-
-		component.find("button").simulate("click");
-		expect(fetchData).toBeCalled();
+			component.find("button").simulate("click");
+			expect(fetchData).toBeCalled();
+		});
 	});
 });
