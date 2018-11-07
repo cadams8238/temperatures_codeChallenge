8447b0a12b34dc65813860b42e67dbfb8cb0edfe
Courtney Adams <cadams8238@gmail.com>
2018-11-06 18:10:32 -0800

    added snapshot test with help from pair programming with Emily

--- src/__tests__/BarGraph.test.js
+++ src/__tests__/BarGraph.test.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import BarGraph from '../components/barGraph';
-import { shallow } from 'enzyme';
+import React from "react";
+import BarGraph from "../components/barGraph";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
-describe('<BarGraph />', () => {
-  it('Should render without crashing', () => {
-    const data = [
-      {
-        date: "1/31",
-        "Max Temp": 45,
-        "Min Temp": 28
-      },
-      {
-        date: "2/1",
-        "Max Temp": 45,
-        "Min Temp": 28
-      }
-    ];
+describe("<BarGraph />", () => {
+	const data = [
+		{
+			date: "1/31",
+			"Max Temp": 45,
+			"Min Temp": 28
+		},
+		{
+			date: "2/1",
+			"Max Temp": 45,
+			"Min Temp": 28
+		}
+	];
 
-    shallow(<BarGraph graphData={data}/>);
-  })
-})
+	it("Should render without crashing", () => {
+		shallow(<BarGraph graphData={data} />);
+	});
+
+	it("Snapshot test", () => {
+		const tree = renderer.create(<BarGraph graphData={data} />).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+});
--- src/__tests__/SearchForm.test.js
+++ src/__tests__/SearchForm.test.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import SearchForm from '../components/searchForm';
-import { shallow } from 'enzyme';
+import React from "react";
+import SearchForm from "../components/searchForm";
+import { shallow, mount } from "enzyme";
 
-describe('<SearchForm />', () => {
-  it('Should render without crashing', () => {
-    const updateSearchTerm = jest.fn(),
-          fetchData = jest.fn(),
-          searchTerm = '';
-    shallow(<SearchForm
-      updateSearchTerm={updateSearchTerm}
-      fetchData={fetchData}
-      searchTerm={searchTerm}
-    />);
-  })
-})
+describe("<SearchForm />", () => {
+	it("Should render without crashing", () => {
+		const updateSearchTerm = jest.fn(),
+			fetchData = jest.fn(),
+			searchTerm = "";
+		shallow(
+			<SearchForm
+				updateSearchTerm={updateSearchTerm}
+				fetchData={fetchData}
+				searchTerm={searchTerm}
+			/>
+		);
+	});
+
+	it("Should call function passed in on button click", () => {
+		const updateSearchTerm = jest.fn(),
+			fetchData = jest.fn(),
+			searchTerm = "",
+			component = mount(
+				<SearchForm
+					updateSearchTerm={updateSearchTerm}
+					fetchData={fetchData}
+					searchTerm={searchTerm}
+				/>
+			);
+		// console.log(component.debug());
+
+		component.find("button").simulate("click");
+		expect(fetchData).toBeCalled();
+	});
+});
